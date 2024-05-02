d2ae58c32be034ab8aaa1f856bbdb02db08ad41d
lnpbqc <60287061+lnpbqc@users.noreply.github.com>
2024-05-02 12:59:10 +0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
+#include "tetromino.h"
+#include "utils.h"
 namespace draw
 {
     void window(int x,int y,int width,int height,std::string title);
+    void tetromino(mino::Tetromino& t,int x,int y);
 } // namespace draw
