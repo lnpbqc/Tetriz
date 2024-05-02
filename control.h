244604d20498b94ab4cb3987debf5817338612de
lnpbqc <60287061+lnpbqc@users.noreply.github.com>
2024-05-02 12:58:44 +0800

    Update control.h

--- control.h
+++ control.h
@@ -3,12 +3,14 @@
 
 #include <map>
 #include <functional>
+#include "tetromino.h"
 
 namespace game
 {
     extern bool _is_running;
     extern char command;
     extern std::map<char,std::function<void()>> common_func;
+    extern mino::Tetromino cur;// 当前操作的方块
 
     extern int _row,_col;
 
